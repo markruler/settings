e3b237128a3eff89f6b8ae4d32ee53f27ae109f1
Changsu Im <imcxsu@gmail.com>
2023-02-23 21:37:18 +0900

    check XDG_CURRENT_DESKTOP is unbound variable

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+# curl -LO https://raw.githubusercontent.com/markruler/settings/main/setup-ubuntu.sh
+# chmod +x setup-ubuntu.sh
+
 # https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
 set -v
 # https://markruler.github.io/posts/shell/minimal-safe-bash-script-template/
@@ -81,33 +84,43 @@ EOL
 
 # SDK Manager
 curl -s "https://get.sdkman.io" | bash
-source "$HOME/.sdkman/bin/sdkman-init.sh"
-sdk version
+# source "$HOME/.sdkman/bin/sdkman-init.sh"
+# # "unbound variable" error occured when using source command
+# sdk version
 
-sdk install java 11.0.11.hs-adpt
-sdk install java 8.0.292.hs-adpt
+# sdk install java 11.0.11.hs-adpt
+# sdk install java 8.0.292.hs-adpt
 
-sdk install maven 3.6.3
-sdk install gradle 6.8.3
+# sdk install maven 3.6.3
+# sdk install gradle 6.8.3
 
 # GNOME Shell
 ## dash-to-dock
-gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.2
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide-in-fullscreen true
-gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.6 # 배경 투명도
-gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 48 # 범위: 16-64 (FHD: 32, QHD: 48, 4K: 64 추천)
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
-gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false # 화면에 아이콘이 꽉 차지 않을 때 여백을 두지 않음
-gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
-gsettings set org.gnome.shell.extensions.dash-to-dock hotkeys-show-dock true
-gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode FOCUS_APPLICATION_WINDOWS
-gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true # 모니터가 여러 개일 때 어느 모니터에서든 dock을 볼 수 있음
-gsettings set org.gnome.shell.extensions.dash-to-dock scroll-action cycle-windows # 아이콘 위에서 마우스 스크롤하면 여러 윈도우를 이동할 수 있음
-gsettings set org.gnome.shell.extensions.dash-to-dock show-favorites true
-gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts true
-gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
-gsettings set org.gnome.shell.extensions.dash-to-dock show-windows-preview true
-gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED # 투명도 모드
-gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style DASHES # 실행 중인 앱 표시 형태
+if [ -z ${XDG_CURRENT_DESKTOP+x} ]; then
+  echo "XDG_CURRENT_DESKTOP is unbound variable"
+else
+  if [[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]]; then
+    # sudo apt-get install -y gnome-shell-extensions
+    # gnome-extensions enable
+
+    gsettings set org.gnome.shell.extensions.dash-to-dock animation-time 0.2
+    gsettings set org.gnome.shell.extensions.dash-to-dock autohide true
+    gsettings set org.gnome.shell.extensions.dash-to-dock autohide-in-fullscreen true
+    gsettings set org.gnome.shell.extensions.dash-to-dock background-opacity 0.6 # 배경 투명도
+    gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 48 # 범위: 16-64 (FHD: 32, QHD: 48, 4K: 64 추천)
+    gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
+    gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+    gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false # 화면에 아이콘이 꽉 차지 않을 때 여백을 두지 않음
+    gsettings set org.gnome.shell.extensions.dash-to-dock force-straight-corner false
+    gsettings set org.gnome.shell.extensions.dash-to-dock hotkeys-show-dock true
+    gsettings set org.gnome.shell.extensions.dash-to-dock intellihide-mode FOCUS_APPLICATION_WINDOWS
+    gsettings set org.gnome.shell.extensions.dash-to-dock multi-monitor true # 모니터가 여러 개일 때 어느 모니터에서든 dock을 볼 수 있음
+    gsettings set org.gnome.shell.extensions.dash-to-dock scroll-action cycle-windows # 아이콘 위에서 마우스 스크롤하면 여러 윈도우를 이동할 수 있음
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-favorites true
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-mounts true
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-trash true
+    gsettings set org.gnome.shell.extensions.dash-to-dock show-windows-preview true
+    gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED # 투명도 모드
+    gsettings set org.gnome.shell.extensions.dash-to-dock running-indicator-style DASHES # 실행 중인 앱 표시 형태
+  fi
+fi
