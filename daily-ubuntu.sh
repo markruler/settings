72c064d5d4798fa2c0a543367ac7729994f89384
Changsu Im <imcxsu@gmail.com>
2023-08-29 16:30:19 +0900

    improve daily-ubuntu script

--- daily-ubuntu.sh
+++ daily-ubuntu.sh
@@ -3,7 +3,7 @@
 # sudo ln -sr daily-ubuntu.sh /usr/local/bin/daily
 
 # https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
-set -v
+#set -v
 # https://markruler.github.io/posts/shell/minimal-safe-bash-script-template/
 set -Eeuo pipefail
 # https://www.gnu.org/software/bash/manual/html_node/Bourne-Shell-Builtins.html#index-trap
@@ -12,10 +12,15 @@ trap - SIGINT SIGTERM ERR EXIT
 
 # Ubuntu 22.04
 # APT
+echo -e "\n🚩 APT update"
 sudo apt-get update
+
+echo -e "\n🚩 APT upgrade"
 sudo apt-get upgrade -y
+
+echo -e "\n🚩 APT autoremove"
 sudo apt-get autoremove -y
 
 ## Snapcraft
+echo -e "\n🚩 Snap refresh"
 sudo snap refresh
-
