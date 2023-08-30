d610210c718d398022e1349a46e39c2c078a9f10
Changsu Im <imcxsu@gmail.com>
2023-08-30 13:17:47 +0900

    update macos script

--- daily-macos.sh
+++ daily-macos.sh
@@ -3,14 +3,18 @@
 # sudo ln -s $PWD/daily-macos.sh /usr/local/bin/daily
 
 # https://www.gnu.org/software/bash/manual/html_node/The-Set-Builtin.html
-set -v
+#set -v
 # https://markruler.github.io/posts/shell/minimal-safe-bash-script-template/
 set -Eeuo pipefail
 # https://www.gnu.org/software/bash/manual/html_node/Bourne-Shell-Builtins.html#index-trap
 # https://www.gnu.org/software/bash/manual/html_node/Signals.html
 trap - SIGINT SIGTERM ERR EXIT
 
+echo -e "\n🚩 Homebrew update"
 brew update
-brew upgrade
-brew cleanup
 
+echo -e "\n🚩 Homebrew upgrade"
+brew upgrade
+
+echo -e "\n🚩 Homebrew cleanup"
+brew cleanup
